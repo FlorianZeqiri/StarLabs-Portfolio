3cb9f8f85c99196b3c6861e32e1ef5874d2b11fd
Florian Zeqiri <florianizeqiri@gmail.com>
2022-11-12 14:19:02 +0100

    restart.
    Start the design all from the beginning.

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,3 @@
-/*
-    Hamburger Menu for the nav
- */
-
-const toggle = document.getElementsByClassName("toggle-btn");
-const item = document.querySelectorAll(".item");
-
-item.forEach(navItem => {
-    function toggleMenu(){
-        if(navItem.classList.contains("active")){
-            navItem.classList.remove("active");
-        }else{
-            navItem.classList.add("active");
-        }
-    }
-
-    document.querySelector(".toggle-btn").addEventListener("click", toggleMenu);
-})
-
 
 /*
     Typing animation

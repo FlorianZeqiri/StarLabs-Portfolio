cb55718e17a763bdabaf9b0e510eaedaa8af77a3
Florian Zeqiri <florianizeqiri@gmail.com>
2022-11-13 19:05:09 +0100

    Style completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,29 @@
+/*
+    Hamburger menu
+ */
+const mainMenu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close-menu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top ='0';
+}
+
+function close(){
+    mainMenu.style.top = '-100%';
+
+}
 
 /*
     Typing animation
  */
 
 var typed = new Typed(".prof-typing",{
-    strings:["","Web Developer", "Student", "Guitar Player"],
+    strings:["","Web Developer", "Student.", "Gutiar Player."],
     typeSpeed: 50,
     backSpeed: 25,
     loop: true
